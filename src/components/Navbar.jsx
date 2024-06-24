bff8449711ad1e8d1347f12bd5f1a9c77700bf74
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-23 19:27:45 -0700

    adding the search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='h-24 w-full'>
       <div className='h-full w-full flex items-center'>
-        <div className='flex justify-center items-center'>
+        <div className='flex gap-2 justify-center items-center'>
             <UtensilsCrossed/>
             <h1 className='text-2xl'>Delicious</h1>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Searchbar from '../components/Searchbar'
+import { Search } from 'lucide-react'
 
 const Home = () => {
   return (
     <div className='h-full w-full'>
       <Navbar/>
-      <Searchbar/>
+      <div className='w-full flex justify-center items-center mt-12'>
+         <div className='flex items-center bg-gray-800 p-2 gap-3 rounded-lg'>
+          <Search className='text-white'/>
+          <input type="text" placeholder='Search..' className=' outline-none border-none bg-transparent text-white w-[400px]'/>
+         </div>
+      </div>
     </div>
   )
 }

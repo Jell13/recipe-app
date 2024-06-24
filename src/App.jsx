a6221e4922fb0b921a4c83c6e50460b2a4d666aa
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-23 19:49:17 -0700

    added vite env variable to test first api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Navbar from "./components/Navbar"
-import Searchbar from "./components/Searchbar"
+
 import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <div className="grainy w-screen h-screen lg:px-32 sm:px-12">
+      <div className="grainy w-screen h-screen lg:px-32 px-12">
         <Home/>
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { Search } from 'lucide-react'
 
 const Home = () => {
+
+  useEffect(() => {
+    getFood()
+  },[])
+
+  const getFood = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&number=3`)
+    const data = await api.json()
+
+    // console.log(data.recipes)
+    data.recipes.map(d => console.log(d.title))
+  }
   return (
     <div className='h-full w-full'>
       <Navbar/>
-      <div className='w-full flex justify-center items-center mt-12'>
+      <div className='w-full flex justify-center items-center mt-10'>
          <div className='flex items-center bg-gray-800 p-2 gap-3 rounded-lg'>
           <Search className='text-white'/>
           <input type="text" placeholder='Search..' className=' outline-none border-none bg-transparent text-white w-[400px]'/>

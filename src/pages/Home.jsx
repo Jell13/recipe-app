527dc3ad36dc0b54d78617761b6c81da77ae2f84
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-30 16:05:26 -0700

    added the different categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
-import { Search } from 'lucide-react'
+import { Beef, Pizza, Salad, Sandwich, Search } from 'lucide-react'
 import { data } from 'autoprefixer'
 import Popular from './Popular'
 
@@ -10,9 +10,25 @@ const Home = () => {
     <div className='h-full w-full'>
       <Navbar/>
       <div className='w-full flex justify-center items-center mt-10'>
-         <div className='flex items-center bg-gray-800 p-2 gap-3 rounded-lg'>
-          <Search className='text-white'/>
-          <input type="text" placeholder='Search..' className=' outline-none border-none bg-transparent text-white w-[400px]'/>
+        <div className='flex flex-col gap-3'>
+          <div className='flex items-center bg-gray-800 p-2 gap-3 rounded-lg'>
+            <Search className='text-white'/>
+            <input type="text" placeholder='Search..' className=' outline-none border-none bg-transparent text-white w-[400px]'/>
+          </div>
+          <div className='flex gap-2 justify-around'>
+            <div className='bg-gray-800 hover:bg-white duration-500 group p-1 rounded-full flex justify-center items-center'>
+              <Sandwich className='text-white group-hover:text-gray-800' width={40} height={40}/>
+            </div>
+            <div className='bg-gray-800 hover:bg-white duration-500 group p-1 rounded-full flex justify-center items-center'>
+              <Beef className='text-white group-hover:text-gray-800' width={40} height={40}/>
+            </div>
+            <div className='bg-gray-800 hover:bg-white duration-500 group p-1 rounded-full flex justify-center items-center'>
+              <Salad className='text-white group-hover:text-gray-800' width={40} height={40}/>
+            </div>
+            <div className='bg-gray-800 hover:bg-white duration-500 group p-1 rounded-full flex justify-center items-center'>
+              <Pizza className='text-white group-hover:text-gray-800' width={40} height={40}/>
+            </div>
+          </div>
          </div>
       </div>
       <Popular/>

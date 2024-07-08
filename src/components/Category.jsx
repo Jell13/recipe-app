e36512089de7cfe4fba74db2418c072fa99346ec
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-08 11:03:43 -0700

    added food based on the cuisine

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,19 +4,19 @@ import { NavLink } from "react-router-dom"
 const Category = () => {
   return (
     <div className="flex gap-4 justify-center items-center text-white">
-      <NavLink to={"/cuisine/American"} className="flex flex-col justify-center items-center bg-gray-800">
+      <NavLink to={"/cuisine/american"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiSteak/>
         <h3>American</h3>
       </NavLink>
-      <NavLink  to={"/cuisine/Italian"} className="flex flex-col justify-center items-center bg-gray-800">
+      <NavLink  to={"/cuisine/italian"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiPizzaSlice/>
         <h3>Italian</h3>
       </NavLink>
-      <NavLink to={"/cuisine/Japanese"} className="flex flex-col justify-center items-center bg-gray-800">
+      <NavLink to={"/cuisine/japanese"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiChopsticks/>
         <h3>Japanese</h3>
       </NavLink>
-      <NavLink to={"/cuisine/Chinese"} className="flex flex-col justify-center items-center bg-gray-800">
+      <NavLink to={"/cuisine/chinese"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiNoodles/>
         <h3>Chinese</h3>
       </NavLink>
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from "framer-motion"
 import { Link, useParams } from "react-router-dom"
 
 const Cuisine = () => {
 
   const params = useParams()
-  console.log(params)
+  const[cuisine, setCuisine] = useState([])
+
+  useEffect(() => {
+    getRecipe(params.type)
+    console.log(cuisine)
+  },[params.type])
+  
+  const getRecipe = async (name) => {
+
+    const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&cuisine=${name}`)
+    const data = await api.json()
+    setCuisine(data.results)
+  }
+
   return (
-    <div>
-      {params.type}
+    <div className='grid items-center justify-center grid-cols-3 gap-12'>
+      {cuisine.map(food => (
+        <div>
+          <img src={food.image} className='rounded-[2rem]' alt="" />
+        </div>
+      ))}
     </div>
   )
 }

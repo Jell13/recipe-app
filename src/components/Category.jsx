b791d47ac9258d9368ca3bea9453008d37f6685e
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-07 19:40:59 -0700

    changing the category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,8 +2,8 @@ import { GiNoodles, GiSteak, GiPizzaSlice, GiChopsticks } from "react-icons/gi"
 
 const Category = () => {
   return (
-    <div className="flex gap-4 justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
+    <div className="flex gap-4 justify-center items-center text-white">
+      <div className="flex flex-col justify-center items-center bg-gray-800">
         <GiSteak/>
         <h3>American</h3>
       </div>

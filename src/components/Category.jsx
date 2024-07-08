224ab1f3201629546574647470659b8fa7152a3d
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-08 10:22:28 -0700

    added the navlinks to go to each pages of cuisine

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,24 +1,25 @@
 import { GiNoodles, GiSteak, GiPizzaSlice, GiChopsticks } from "react-icons/gi"
+import { NavLink } from "react-router-dom"
 
 const Category = () => {
   return (
     <div className="flex gap-4 justify-center items-center text-white">
-      <div className="flex flex-col justify-center items-center bg-gray-800">
+      <NavLink to={"/cuisine/American"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiSteak/>
         <h3>American</h3>
-      </div>
-      <div className="flex flex-col justify-center items-center">
+      </NavLink>
+      <NavLink  to={"/cuisine/Italian"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiPizzaSlice/>
         <h3>Italian</h3>
-      </div>
-      <div className="flex flex-col justify-center items-center">
+      </NavLink>
+      <NavLink to={"/cuisine/Japanese"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiChopsticks/>
         <h3>Japanese</h3>
-      </div>
-      <div className="flex flex-col justify-center items-center">
+      </NavLink>
+      <NavLink to={"/cuisine/Chinese"} className="flex flex-col justify-center items-center bg-gray-800">
         <GiNoodles/>
         <h3>Chinese</h3>
-      </div>
+      </NavLink>
     </div>
   )
 }
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { motion } from "framer-motion"
+import { Link, useParams } from "react-router-dom"
 
 const Cuisine = () => {
+
+  const params = useParams()
+  console.log(params)
   return (
     <div>
-      
+      {params.type}
     </div>
   )
 }
